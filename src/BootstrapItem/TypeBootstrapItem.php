24bd61e1b158202d2f2c4ebafca80b4d195c6180
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-03-15 15:25:11 +0100

    new bootstraping

---
+++ src/BootstrapItem/TypeBootstrapItem.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * kiwi-suite/entity (https://github.com/kiwi-suite/entity)
+ *
+ * @package kiwi-suite/entity
+ * @see https://github.com/kiwi-suite/entity
+ * @copyright Copyright (c) 2010 - 2018 kiwi suite GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+namespace KiwiSuite\Entity\BootstrapItem;
+
+use KiwiSuite\Contract\Application\BootstrapItemInterface;
+use KiwiSuite\Contract\Application\ConfiguratorInterface;
+use KiwiSuite\Entity\Type\TypeConfigurator;
+
+class TypeBootstrapItem implements BootstrapItemInterface
+{
+    /**
+     * @return ConfiguratorInterface
+     */
+    public function getConfigurator(): ConfiguratorInterface
+    {
+        return new TypeConfigurator();
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getVariableName(): string
+    {
+        return 'type';
+    }
+
+    /**
+     * @return string
+     */
+    public function getFileName(): string
+    {
+        return 'type.php';
+    }
+}
---
+++ src/Type/TypeConfigurator.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * kiwi-suite/entity (https://github.com/kiwi-suite/entity)
+ *
+ * @package kiwi-suite/entity
+ * @see https://github.com/kiwi-suite/entity
+ * @copyright Copyright (c) 2010 - 2018 kiwi suite GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+namespace KiwiSuite\Entity\Type;
+
+use KiwiSuite\Contract\Application\ConfiguratorInterface;
+use KiwiSuite\Contract\Application\ServiceRegistryInterface;
+use KiwiSuite\ServiceManager\Factory\AutowireFactory;
+use KiwiSuite\ServiceManager\SubManager\SubManagerConfigurator;
+
+final class TypeConfigurator implements ConfiguratorInterface
+{
+    /**
+     * @var SubManagerConfigurator
+     */
+    private $subManagerConfigurator;
+
+    /**
+     * MiddlewareConfigurator constructor.
+     */
+    public function __construct()
+    {
+        $this->subManagerConfigurator = new SubManagerConfigurator(TypeSubManager::class, TypeInterface::class);
+    }
+
+    /**
+     * @param string $directory
+     * @param bool $recursive
+     */
+    public function addDirectory(string $directory, bool $recursive = true): void
+    {
+        $this->subManagerConfigurator->addDirectory($directory, $recursive);
+    }
+
+    /**
+     * @param string $action
+     * @param string $factory
+     */
+    public function addType(string $action, string $factory = AutowireFactory::class): void
+    {
+        $this->subManagerConfigurator->addFactory($action, $factory);
+    }
+
+    /**
+     * @param ServiceRegistryInterface $serviceRegistry
+     */
+    public function registerService(ServiceRegistryInterface $serviceRegistry): void
+    {
+        $this->subManagerConfigurator->registerService($serviceRegistry);
+    }
+}
