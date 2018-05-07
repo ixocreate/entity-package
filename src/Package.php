e5ffe727a9dbb1a649c461164d513b7f2378b766
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-05-07 12:20:36 +0200

    CS

--- src/Package.php
+++ src/Package.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * kiwi-suite/entity (https://github.com/kiwi-suite/entity)
+ *
+ * @package kiwi-suite/entity
+ * @see https://github.com/kiwi-suite/entity
+ * @copyright Copyright (c) 2010 - 2018 kiwi suite GmbH
+ * @license MIT License
+ */
+
 declare(strict_types=1);
 
 namespace KiwiSuite\Entity;
